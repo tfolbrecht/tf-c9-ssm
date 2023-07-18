cd3ef16860b5c97669daed9d4a2d02ae3294472a
tfolbrecht <tfolbrecht@gmail.com>
2023-07-18 00:46:00 -0400

    Remove state bucket oops

--- main.tf
+++ main.tf
@@ -1,9 +1,4 @@
 terraform {
-  backend "s3" {
-    bucket = "storefront-tfstate"    # Explicate, cannot use vars, avoid circular deps
-    key    = "c9-experiment.tfstate" # changing this key will require a state migration
-    region = "us-east-1"
-  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
